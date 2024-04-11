df9c9ad00cf23e0230b5120a287bfc5831183742
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-04-11 12:49:01 -0700

    Adjust input variables for predict

--- api/api_v1/predict.py
+++ api/api_v1/predict.py
@@ -10,11 +10,7 @@ def predict(
     assessed_improvements: float,
     last_sold: str,
     assessed_land_2022: float,
-    assessed_total_2021: float,
-    assessed_improvements_2022: float,
-    gross_taxes: float,
-    latitude: float,
-    assessed_land_2021: float
+    assessed_total_2021: float
 ):
     input_data = {
         '2023 Assessed Land': assessed_land,
@@ -22,10 +18,6 @@ def predict(
         '2023 Assessed Improvements': assessed_improvements,
         'Last Sold': last_sold,
         '2022 Assessed Land': assessed_land_2022,
-        '2021 Assessed Total': assessed_total_2021,
-        '2022 Assessed Improvements': assessed_improvements_2022,
-        '2023 Gross Taxes': gross_taxes,
-        'Latitude': latitude,
-        '2021 Assessed Land': assessed_land_2021
+        '2021 Assessed Total': assessed_total_2021
     }
     return {"Data": use_model(input_data)}
