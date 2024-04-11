95ab33ebfdf3b8909390f3904d713427a2c2a392
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-04-11 02:50:45 -0700

    Configure CORS

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from mangum import Mangum
 from api.api_v1.api import router as api_router
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main():
